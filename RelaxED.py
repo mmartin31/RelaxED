d2edc288796c7eb9c9507791cbea8fc21b4b9075
mmart31 <mmartin31@gardner-webb.edu>
2024-09-21 18:37:54 -0400

    Initial Commit

---
+++ RelaxED.py
@@ -0,0 +1,22 @@
+from kivy.app import App
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.screenmanager import ScreenManager, Screen, TransitionBase
+
+class ScreenManagement(ScreenManager):
+    pass
+
+class LoadingScreen(Screen):
+    pass
+
+class HomeScreen(Screen):
+    pass
+
+
+class TestApp(App):
+    def build(self):
+        self.fl = FloatLayout()
+        return ScreenManagement()
+
+TestApp().run()
+
+
